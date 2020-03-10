ee80abbb24654ff21dc1e299810d7850d5578255
gabrielecarru <gabrielecarru@mbp-di-gabriele>
2020-03-10 17:27:54 +0100

    Commit progetto terminato 'Buongiorno/Ciao, Nome!

---
+++ src/main/java/it/polito/tdp/JavaFXCrashcourse/Main.java
@@ -0,0 +1,13 @@
+package it.polito.tdp.JavaFXCrashcourse;
+
+/* Main diverso dalle slide a causa di una versione più recente di Java (11) 
+ * Chiama un metodo Main di un'altra classe per questioni di visibilità.
+ * Si lavora in 'EntryPoint'.
+ */
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
+
